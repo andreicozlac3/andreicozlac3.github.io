2ab4146f584ef0188f74ccef9030377ca61cef70
hariveliki <98284163+hariveliki@users.noreply.github.com>
2024-06-19 19:57:28 +0000

    Refactor footer and courses page

--- src/components/courseContent/CourseContent.jsx
+++ src/components/courseContent/CourseContent.jsx
@@ -6,10 +6,13 @@ class CourseContent extends Component {
     return (
       <div class="container">
         <div class="course-item">
-          <img src="image1.jpg" alt="Course 1" />
+          <img
+            src="https://th.bing.com/th/id/OIP.xkJHbFx-4usyzMNWf37gTQAAAA?w=226&h=180&c=7&r=0&o=5&dpr=2&pid=1.7"
+            alt="Course 1"
+          />
           <div class="course-description">
-            <h3>Course Title 1</h3>
-            <p>Brief description of Course 1.</p>
+            <h3>Title</h3>
+            <p>Description</p>
           </div>
         </div>
       </div>
